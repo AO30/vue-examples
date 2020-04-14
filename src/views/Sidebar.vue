7edae5c8bb267c7262ac9bc42446b52e8ea6b33d
AO30 <3080493008@qq.com>
2020-04-14 09:51:00 +0800

    computed02

---
+++ src/views/Sidebar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Vue示例</h1>
+    <ul>
+      <li><router-link to="/example01">Helloworld</router-link></li>
+      <li><router-link to="/example02">数据驱动&计算属性</router-link></li>
+    </ul>
+  </div>
+</template>
+<style scoped>
+h1 {
+  color: red;
+}
+ul {
+  margin-top: 00px;
+}
+</style>
